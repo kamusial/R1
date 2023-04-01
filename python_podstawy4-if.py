129473667cbf3e8368ec8eda09dc959b5f0c3ad7
kamusial <kamil.support@gmail.com>
2023-04-01 11:04:28 +0200

    Add files via upload

---
+++ python_podstawy4-if.py
@@ -0,0 +1,12 @@
+x = int(input('Wpisz liczbe '))
+if (x > 5):
+    print('Liczba ',x,'jest wieksza od 5')
+    print('Testowa linia 1')
+else:
+    print('Liczba ',x,'jest NIE wieksza od 5')
+    print('Testowa linia 2')
+print('Testowa linia 3')
+
+#program pyta o zarobki. Jeśli zarabiasz więcej niż 4000zł
+#nalicza podatek 15%, w przeciwnym wypadku, podatek 5%
+#program wypisuje kwotę pdoatku do zaplaty
