9f90e871e38e6338edce61ceaf1fdb36b3a776e5
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-04-02 13:47:58 +0200

    1

--- 10_biblioteki_time_i_os.py
+++ 10_biblioteki_time_i_os.py
@@ -1,3 +1,10 @@
 import os
-
-
+import time
+print('aktualna lokalizacja to:',os.getcwd())
+os.chdir('C:\\Users\\vdi-student\\Desktop\\Kamil')
+print('aktualna lokalizacja to:',os.getcwd())
+os.mkdir('new_folder')
+time.sleep(2)
+os.rename('new_folder', '2_new_folder')
+time.sleep(2)
+os.rmdir('2_new_folder')
